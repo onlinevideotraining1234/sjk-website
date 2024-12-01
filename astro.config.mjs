814450fc4ad73eb2f682d065f508f2a22c691e93
Ganzorig Urtnasan <gzg_u@yahoo.com>
2024-12-01 19:16:24 +0800

    4 page-ee nemsen

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()],
+  image: {
+    service: {
+      type: "astro/assets",
+    },
+  },
 });
